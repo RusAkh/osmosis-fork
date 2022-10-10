d96314bd8c03a6616cf56e29a21570cb5131f764
Ruslan Akhtariev <ruslanakhtariev@Ruslans-MacBook-Air.local>
2022-10-10 18:12:43 +0700

    #2971

--- x/superfluid/client/cli/cli_test.go
+++ x/superfluid/client/cli/cli_test.go
@@ -1,1 +1,47 @@
 package cli_test
+
+import (
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/testutil/network"
+	"github.com/stretchr/testify/suite"
+
+	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
+
+	"github.com/osmosis-labs/osmosis/v12/app"
+	"github.com/osmosis-labs/osmosis/v12/x/superfluid/client/cli"
+)
+
+type IntegrationTestSuite struct {
+	suite.Suite
+
+	cfg     network.Config
+	network *network.Network
+}
+
+func (s *IntegrationTestSuite) SetupSuite() {
+	s.T().Log("setting up integration test suite")
+
+	s.cfg = app.DefaultConfig()
+	s.network = network.New(s.T(), s.cfg)
+
+	_, err := s.network.WaitForHeight(1)
+	s.Require().NoError(err)
+
+	val := s.network.Validators[0]
+	s.Require().NotNil(val)
+}
+func (s *IntegrationTestSuite) TestGetCmdQueryParams() {
+	val := s.network.Validators[0]
+
+	cmd := cli.GetCmdQueryParams()
+	clientCtx := val.ClientCtx
+
+	out, err := clitestutil.ExecTestCLICmd(clientCtx, cmd, []string{})
+	s.Require().NoError(err)
+	s.Require().NotNil(out)
+}
+
+func TestIntegrationTestSuite(t *testing.T) {
+	suite.Run(t, new(IntegrationTestSuite))
+}
