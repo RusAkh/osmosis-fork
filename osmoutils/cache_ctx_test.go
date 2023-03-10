54def847c358c17c9745f81298886ef241a27304
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-03-10 11:19:59 +0700

    change

--- osmoutils/cache_ctx_test.go
+++ osmoutils/cache_ctx_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/osmosis-labs/osmosis/osmoutils"
 )
 
-// change in osmoutils 3
+// change in osmoutils 4
 var expectedOutOfGasError = types.ErrorOutOfGas{Descriptor: "my func"}
 
 func consumeGas(ctx sdk.Context, gas uint64, numTimes int) error {
