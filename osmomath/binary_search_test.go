4d8652c848ed5c3d64bd3361af5ffef84a1afd14
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-05-12 12:54:30 +0700

    change forced

--- osmomath/binary_search_test.go
+++ osmomath/binary_search_test.go
@@ -15,7 +15,7 @@ var (
 	zero          = ZeroDec()
 )
 
-// forced osmomath change 1
+// forced osmomath change 2
 func TestBinarySearch(t *testing.T) {
 	// straight line function that returns input. Simplest to binary search on,
 	// binary search directly reveals one bit of the answer in each iteration with this function.
